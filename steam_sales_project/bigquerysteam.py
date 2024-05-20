3500152f32c4b12c525d191d444bc7387b398712
matheuschere <matheuschere@hotmail.com>
2024-05-20 15:43:15 -0300

    One data
    Could only get one game data

---
+++ steam_sales_project/bigquerysteam.py
@@ -0,0 +1,23 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file(
+    'credentials.json'
+)
+
+import pandas as pd
+
+client = bigquery.Client(credentials=credentials)
+
+
+project_id = 'projetosql-423912'
+dataset_id = '1'
+table_id = 'data_steam'
+
+df = pd.read_csv('steam_game_info.csv')
+
+job = client.load_table_from_dataframe(df, f'{project_id}.{dataset_id}.{table_id}')
+
+job.result()
+
+print("Dados carregados com sucesso no BigQuery")
---
+++ steam_sales_project/scrape_steamdb.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://store.steampowered.com/app/1150530/'
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    name = soup.find('div', class_='apphub_AppName').text.strip()
+    price = soup.find('div', class_='game_purchase_price').text.strip()
+    discount = soup.find('div', class_='discount_pct').text.strip()
+    rating = soup.find('span', class_='game_review_summary').text.strip()
+    release_date = soup.find('div', class_='date').text.strip()
+
+    df = pd.DataFrame({
+        'name': [name],
+        'price': [price],
+        'discount': [discount],
+        'rating': [rating],
+        'release_date': [release_date]
+    })
+
+    df.to_csv('steam_game_sales.csv', index=False)
+
+    print("Dados extraídos e salvos com sucesso em 'steam_game_sales.csv'")
+
+except Exception as e:
+    print("Erro ao processar a requisição:", e)
